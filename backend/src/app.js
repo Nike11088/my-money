bef8e684db908de61681dc528c08cebf70d93217
Nikita Abakumov <abakumov-n@mail.ru>
2025-08-23 19:21:59 +0300

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,23 +2,26 @@ import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import router from './routes/index.js'
 import getSpecs from './swagger/swagger.js'
-// import indexRouter from './routes/index.js'
-// import booksRouter from './routes/books.js'
+import cors from 'cors'
+import indexRouter from './routes/index.js'
+import booksRouter from './routes/books.js'
 
 const app = express()
+
 const PORT = process.env.PORT || 3000
 
-app.use(express.json())
+// Middleware
+app.use(cors())
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api', router)
+app.use('/api/index', indexRouter)
+app.use('/api/books', booksRouter)
 
 // Роут для документации Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(getSpecs({ port: PORT })))
-
-// Подключаем роутеры
-// app.use('/api/index', indexRouter)
-// app.use('/api/books', booksRouter)
 
 // Запуск сервера
 app.listen(PORT, () => {
