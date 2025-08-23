e72916b649149192e69c8881ed883019e8a7744b
Nikita Abakumov <abakumov-n@mail.ru>
2025-08-23 19:56:30 +0300

    add Access-Control-Allow headers

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,13 @@ app.use(cors())
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
+
 // Routes
 app.use('/api', router)
 app.use('/api/index', indexRouter)
